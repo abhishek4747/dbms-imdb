1293b023f8148800b9e3f08588faca07edd4c678
Abhishek Kumar <abhishek.iitd16@gmail.com>
2014-04-17 14:25:44 +0530

    Initial commit for imdb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Imdb::Application.config.secret_key_base = 'ac5d084246e99c89b3b46b14cb60b6fe5919d51cc9af179616bcb1bcb4cae790b8d67c634f891f4964f3f227bc7fce426c34f71aab14f91fecfd9f5c8d3d6a5e'
